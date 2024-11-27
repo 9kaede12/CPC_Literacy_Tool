a0956afab4912ac5dfea61a056a5b66d512c1a61
9kaede12 <169217935 +9kaede12@users.noreply.github.com>
2024-11-27 19:21:21 +0900

    コメント追加

--- jobSelect/js/jobSelect.js
+++ jobSelect/js/jobSelect.js
@@ -1,7 +1,9 @@
+// 確認画面に遷移するときに職業名と職業画像を一緒に渡す
 function confirmSelection(jobName, imgSrc) {
   window.location.href = `confirm.html?job=${encodeURIComponent(jobName)}&img=${encodeURIComponent(imgSrc)}`;
 }
 
+// 戻るボタンが押された時の遷移先ファイル
 function goBack() {
   window.location.href = "index.html";
 }
